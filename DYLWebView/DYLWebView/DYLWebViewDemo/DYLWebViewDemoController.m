0307713832f0d542a456ba23b53739f371170b97
lostknight <diaozheng@mannyi.com>
2017-07-27 10:03:37 +0800

    加上类似微信下拉WebView显示网页支持信息的小功能。

--- DYLWebView/DYLWebView/DYLWebViewDemo/DYLWebViewDemoController.m
+++ DYLWebView/DYLWebView/DYLWebViewDemo/DYLWebViewDemoController.m
@@ -14,6 +14,7 @@
 
 @property (strong, nonatomic) DYLWebView *webView;
 @property (strong, nonatomic) DYLWebViewProgressView *progressBarView;
+@property (strong, nonatomic) UILabel *supportLabel;
 
 @end
 
@@ -25,16 +26,29 @@
     self.navigationItem.title = @"测试";
     self.view.backgroundColor = [UIColor whiteColor];
     
+    self.supportLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, [self isNavigationHidden]?10:74, CGRectGetWidth(self.view.bounds), 25)];
+    
+    _supportLabel.font = [UIFont systemFontOfSize:14.0f];
+    _supportLabel.textColor = [UIColor blackColor];
+    _supportLabel.textAlignment = NSTextAlignmentCenter;
+    
     self.webView = [[DYLWebView alloc] initWithFrame:self.view.bounds];
     _webView.delegate = self;
     [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.mannyi.com"]]];
     [self.view addSubview:_webView];
     
+    [_webView.realWebView insertSubview:_supportLabel belowSubview:_webView.scrollView];
+    
+    __weak typeof(self) weakSelf = self;
     self.progressBarView = [[DYLWebViewProgressView alloc] initWithFrame:CGRectMake(0, [self isNavigationHidden]?0:64, self.view.frame.size.width, 2)];
     _progressBarView.progressColor = [UIColor greenColor];
+    
+    // progress到达1.0之后，会执行该block
+    _progressBarView.completionBlock = ^{
+        weakSelf.supportLabel.text = @"该网页由XXX平台提供";
+    };
     [self.view addSubview:_progressBarView];
     
-    NSLog(@"VC -- %@", _webView.realWebView);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -56,6 +70,7 @@
 {
     [super viewWillAppear:animated];
     self.navigationController.navigationBar.barTintColor = [UIColor whiteColor];
+    _webView.scrollView.contentInset = [self isNavigationHidden]?UIEdgeInsetsZero:UIEdgeInsetsMake(64, 0, 0, 0);
 }
 
 - (BOOL)webView:(DYLWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(DYLWebViewNavigationType)navigationType
@@ -89,9 +104,14 @@
     _progressBarView.progress = progress;
 }
 
+- (void)webView:(DYLWebView *)webView webViewForTitle:(NSString *)title
+{
+    self.navigationItem.title = title;
+}
+
 - (BOOL)isNavigationHidden
 {
-    return !self.navigationController || !self.navigationController.navigationBar.translucent || !self.navigationController.navigationBar;
+    return !self.navigationController || self.navigationController.navigationBar.hidden || !self.navigationController.navigationBar.translucent || !self.navigationController.navigationBar;
 }
 
 @end
